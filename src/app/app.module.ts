80025ecee33959e7ef4ecee1bd6edca5715cd81f
santiago guichard <guichardsantiago@gmail.com>
2020-10-12 18:50:11 -0300

    first complete version of cv with timeline, languages ranking and about me secton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent, BodyComponent } from 'src/components/index';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatIconModule, MatDividerModule} from '@angular/material';
+import { MatIconModule, MatDividerModule, MatStepperModule} from '@angular/material';
+import { RatingComponent } from 'src/components/rating';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    BodyComponent
+    BodyComponent,
+    RatingComponent
   ],
   imports: [
     MatIconModule,
     MatDividerModule,
+    MatStepperModule,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule
